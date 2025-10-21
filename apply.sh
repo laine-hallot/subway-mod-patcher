a3b810227bf6ec398f0765d0cd889ca8d2fadad6
Laine Hallot <lainehallot@gmail.com>
2025-10-20 22:03:18 -0500

    feat: create patch manifest format

--- apply.sh
+++ apply.sh
@@ -1,4 +1,33 @@
+patchDir () {
+  echo $1
+  ROOT_DIR=$(pwd)
+  NAME=$(cat $1/patch-manifest.json | jq  '.patchName' --raw-output)
+  if [[ !$? -eq 0 ]]; then
+    echo "$1/patch-manifest.json patchName is undefined"
+  fi
+    VERSION=$(cat $1/patch-manifest.json | jq  '.gameVersion' --raw-output)
+  if [[ !$? -eq 0 ]]; then
+    echo "$1/patch-manifest.json patchName is undefined"
+  fi
+  COMMAND=$(cat $1/patch-manifest.json | jq '.applyPatch' --raw-output)
+  if [[ !$? -eq 0 ]]; then
+    echo "$1/patch-manifest.json patchName is undefined"
+  fi
+  DIR=$(cat $1/patch-manifest.json | jq '.workingDir' --raw-output)
+  if [ -z $DIR ]; then
+    echo "Installing patch: $NAME using working directory: $DIR"
+    cd $DIR
+    $COMMAND
+  else
+    cd $1
+    echo "Installing patch: $NAME"
+    $COMMAND
+  fi
+  cd $ROOT_DIR
+} 
+
 
 echo "Patching game files"
-patch -p1 tmp/asar-extract/dist/main/main.js ./patches/titlebar.patch
-patch -p1 tmp/app-image/metro-maker4.desktop ./patches/wayland.patch
+export -f patchDir
+PATCH_DIRECTORIES=$(find ./patches/ -maxdepth 1 -type d -exec sh -c '[ -f "$0"/patch-manifest.json ]' '{}' \; -print)
+echo "$PATCH_DIRECTORIES" | xargs -P 24 -I '{}' bash -c 'patchDir "$@"' _ {}
